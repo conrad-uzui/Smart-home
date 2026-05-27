dd432218e3851e3258a876bdea6bebd89e00a299
Nathaniel Agbemava <nathanielagbemava@gmail.com>
2026-05-27 08:48:11 +0000

    created the post request

--- src/devices/devices.controller.ts
+++ src/devices/devices.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller,Post,Body } from '@nestjs/common';
+import { CreateDeviceDto } from 'src/Type/dto/createdevice.dto';
+import { DevicesService } from './devices.service';
 
 @Controller('devices')
-export class DevicesController {}
+export class DevicesController {
+
+  constructor(private readonly devicesService:DevicesService){}
+
+@Post()
+create(@Body() dto:CreateDeviceDto){
+return this.devicesService.create(dto);
+}
+
+}
--- src/devices/devices.service.ts
+++ src/devices/devices.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { Device } from 'src/Type/types/device.type';
+import { CreateDeviceDto } from 'src/Type/dto/createdevice.dto';
+import { randomUUID } from 'crypto';
+import { DeviceStatus } from 'src/Type/enums/device-status.enum';
 
 @Injectable()
 export class DevicesService {
   private devices :Device[]=[];
 
+  create(createdevicedto:CreateDeviceDto){
+  const newDevice ={
+    id:randomUUID(),
+    ...createdevicedto,
+    status:DeviceStatus.OFF
+
+  }
+  
+  this.devices.push(newDevice);
+  return newDevice;
+  }
 }
