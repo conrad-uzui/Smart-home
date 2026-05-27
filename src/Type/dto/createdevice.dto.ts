88d6ed16244390f8608c28051dde9d270731253c
Nathaniel Agbemava <nathanielagbemava@gmail.com>
2026-05-27 07:41:11 +0000

    added class validation

--- src/Type/dto/createdevice.dto.ts
+++ src/Type/dto/createdevice.dto.ts
@@ -1,7 +1,15 @@
 import { DeviceType } from "../enums/device-type.enum";
+import { IsString, IsNotEmpty, IsEnum} from 'class-validator';
 
 export class CreateDeviceDto {
+  @IsString()
+  @IsNotEmpty()
   name:string;
+
+  @IsString()
+  @IsNotEmpty()
   room:string;
+
+  @IsEnum(DeviceType)
   type:DeviceType;
 }
